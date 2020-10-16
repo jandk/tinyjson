5ce41e4dda0f26e9c9f7cf2ec3f113195e33b0e1
Jan De Kock <jan@twofold.be>
2020-10-16 11:48:22 +0200

    Add missing @FunctionalInterface

--- bind/src/main/java/be/twofold/json/JsonSerializer.java
+++ bind/src/main/java/be/twofold/json/JsonSerializer.java
@@ -2,6 +2,7 @@ package be.twofold.json;
 
 import be.twofold.tinyjson.*;
 
+@FunctionalInterface
 public interface JsonSerializer<T> {
 
     JsonValue serialize(T object, JsonSerializationContext context);
